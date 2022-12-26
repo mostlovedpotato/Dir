e621bda718a32109d2c5f3dd504c8b76f32fde0a
mostlovedpotato <mostlovedpotato@gmail.com>
2022-12-26 17:07:49 +0530

    anti-diagnoal print

---
+++ 2022/December/26/anti_diagonal_print.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include<iostream>
+
+
+using namespace std;
+
+void diagonal_print(vector<vector<int>>& A) {
+    int n = A.size();
+    int N = 2 * n - 1;
+    vector<vector<int>> res(N);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            res[i + j].push_back(A[i][j]);
+        }
+    }
+    for (int i = 0; i < res.size(); i++) {
+        cout << endl;
+        for (int j = 0; j < res[i].size(); j++) {
+            cout << res[i][j] << " ";
+        }
+    }
+}
+
+int main()
+{
+    int n; cin >> n;
+    vector<vector<int>> A(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+    diagonal_print(A);
+
+    return 0;
+}
